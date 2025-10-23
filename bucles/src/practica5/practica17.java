c64d64fc2840744e3412e7ef37b23e4c36ef8c4d
jose <jose@practica.com>
2025-10-23 20:34:54 +0200

    Primer commit desde eclipse

---
+++ bucles/src/practica5/practica17.java
@@ -0,0 +1,15 @@
+package practica5;
+
+public class practica17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Repetir el ejemplo del "simulador de calculadora" para poder realizar
+		 * todas las operaciones que el usuario quiera.
+		 */
+
+	}
+
+}
---
+++ bucles/src/practica5/practica_2.java
@@ -0,0 +1,34 @@
+package practica5;
+
+import java.util.Scanner;
+
+public class practica_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Leer un número e indicar si es positivo o negativo. El proceso se repetirá
+		 * hasta que se introduzca un 0.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+
+		int num;
+
+		do {
+			System.out.println("di un numero");
+			num = sc.nextInt();
+
+			if (num > 0)
+				System.out.println("es positivo");
+			else
+				System.out.println("es negativo");
+
+		} while (num != 0);
+
+		System.out.println("Fin del programa");
+
+	}
+
+}
